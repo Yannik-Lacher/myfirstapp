8883b221313f2be933b60eef00b4797d7ea9eed9
Yannik Lacher <yanniklacher@lxbn0116s-mbp.localdomain>
2021-12-16 16:43:10 +0100

    CircleCI Testing

--- build/build_script.sh
+++ build/build_script.sh
@@ -12,7 +12,7 @@ export SYNC_URL="https://${HOST}/services/integration-api/v1/synchronizationRuns
 TOKEN=$(curl -X POST --url https://${HOST}/services/mtm/v1/oauth2/token -u apitoken:${MI_DEV_TOKEN} --data grant_type=client_credentials | jq -r '.access_token') 
 
 # Run license-checker
-license-checker --json > $TRAVIS_BUILD_DIR/build/dependencies.json
+license-checker --json > $CIRCLE_WORKING_DIRECTORY/build/dependencies.json
 
 # API call to send the manifest file, dependencies and metadata to workspace
 # Required input data in the API call:
@@ -24,8 +24,8 @@ curl -X POST \
   -H 'Cache-Control: no-cache' \
   -H "Authorization: Bearer ${TOKEN}" \
   -H 'Content-Type: multipart/form-data' \
-  -F dependencies=@"$TRAVIS_BUILD_DIR/build/dependencies.json" \
-  -F manifest=@"$TRAVIS_BUILD_DIR/lx-manifest.yaml" \
+  -F dependencies=@"$CIRCLE_WORKING_DIRECTORY/build/dependencies.json" \
+  -F manifest=@"$CIRCLE_WORKING_DIRECTORY/lx-manifest.yaml" \
   -F 'data={
   "version": "1.1.0",
   "stage": "dev",
